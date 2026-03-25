4179404fc0d0234fd2a9875f631c0c8db34febc9
BrysonT21 <thorntonb867@gmail.com>
2026-03-25 20:43:46 +0000

    Update CalculatorTest.java

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
-    Calculator calc = new Calculator();
+    Calculator calc = new Calculator();// Comment again
 
     @Test void testAdd() { assertEquals(5, calc.add(2, 3)); }
     @Test void testDivideByZero() { assertThrows(IllegalArgumentException.class, () -> calc.divide(10, 0)); }
